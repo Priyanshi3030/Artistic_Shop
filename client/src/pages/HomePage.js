a27325bcc88d81dff080e60215dea2b6a2152710
priyanshi <priyashivhare2003@gmail.com>
2024-01-14 14:52:02 +0530

    updated

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -43,7 +43,7 @@ const HomePage = () => {
     try {
       setLoading(true);
       console.log(`${process.env.REACT_APP_API}`);
-      const { data } = await axios.get(`https://artistic-shop-backend.onrender.com/api/v1/product/product-list/${page}`);
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`);
       setLoading(false);
       setProducts(data.products);
     } catch (error) {
@@ -55,7 +55,7 @@ const HomePage = () => {
   //getTOtal COunt
   const getTotal = async () => {
     try {
-      const { data } = await axios.get(`https://artistic-shop-backend.onrender.com/api/v1/product/product-count`);
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-count`);
       setTotal(data?.total);
     } catch (error) {
       console.log(error);
@@ -70,7 +70,7 @@ const HomePage = () => {
   const loadMore = async () => {
     try {
       setLoading(true);
-      const { data } = await axios.get(`https://artistic-shop-backend.onrender.com/api/v1/product/product-list/${page}`);
+      const { data } = await axios.get(`${process.env.REACT_APP_API}/api/v1/product/product-list/${page}`);
       setLoading(false);
       setProducts([...products, ...data?.products]);
     } catch (error) {
@@ -100,7 +100,7 @@ const HomePage = () => {
   //get filterd product
   const filterProduct = async () => {
     try {
-      const { data } = await axios.post(`https://artistic-shop-backend.onrender.com/api/v1/product/product-filters`, {
+      const { data } = await axios.post(`${process.env.REACT_APP_API}api/v1/product/product-filters`, {
         checked,
         radio,
       });
@@ -158,7 +158,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`https://artistic-shop-backend.onrender.com/api/v1/product/product-photo/${p._id}`}
+                  src={`${process.env.REACT_APP_API}api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
